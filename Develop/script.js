008db84413a35b4f523176ec01b82163029e14fc
Sam Moore <sammooresemail23@gmail.com>
2022-07-09 16:43:19 -0700

    Javascript variables

--- Develop/script.js
+++ Develop/script.js
@@ -15,46 +15,14 @@ var blankVariable = "";
 // Write password to the #password input
 function writePassword() {
 
-  // design the password via prompts and user data entry
+  // design the password via prompts and user data entry fom the user.
   var lowerChoice = confirm("Would you like your password to contain lowercase characters?");
   var upperChoice = confirm("Would you like your password to contain uppercase characters?");
   var numChoice = confirm("Would you like your password to include numbers as characters?");
   var specialChoice = confirm("Would you like your password to contain special characters?");
   var passCharNum = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
 
-  // if statement to specify parameters
-  if (passCharNum < 8 || passCharNum > 128) {
-    alert("The password is not between the parameteres 8 and 128. Please try again.");
-    var passCharNum = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
-
-}
-else if (lowerChoice === false && upperChoice === false && numChoice && specialChoice === false){
-  alert("You must select at least 1 option to generate a password.");
-  var lowerChoice = confirm("Would you like your password to contain lowercase characters?");
-  var upperChoice = confirm("Would you like your password to contain uppercase characters?");
-  var numChoice = confirm("Would you like your password to include numbers as characters?");
-  var specialChoice = confirm("Would you like your password to contain special characters?");
-};
-
-// if statements to specify parameters
-if (upperChoice){
-  blankVariable = blankVariable.concat(upper);
-}
-
-if (lowerChoice){
-  blankVariable = blankVariable.concat(lower);
-}
-
-if (numChoice){
-  blankVariable = blankVariable.concat(upper);
-}
-
-if (specialChoice){
-  blankVariable = blankVariable.concat(upper);
-}
-//console.log(blankVariable);
-
-// Loop through the 
+  
   var password = generatePassword() ;
   var passwordText = document.querySelector("#password");
 
