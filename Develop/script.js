0e6c6dced70ae99ef679d4f59a3fabe047354abd
Sam Moore <sammooresemail23@gmail.com>
2022-07-09 20:38:17 -0700

    javascript progress

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,13 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //begin variable creation for later use
-var special = [" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var lower = ["abcdefghijklmnopqrstuvwxyz"];
-var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var number = ["0123456789"];
-var blankVariable = [];
-
+var special = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var holderArray = [];
+var holderArray2 = [];
 
   // design the password via prompts and user data entry fom the user.
   var lowerChoice = confirm("Would you like your password to contain lowercase characters?");
@@ -20,13 +17,16 @@ var blankVariable = [];
   var specialChoice = confirm("Would you like your password to contain special characters?");
   var passCharNum = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
 
+  function writePassword() {
+
   // if statement to specify parameters to the user.
   if (passCharNum < 8 || passCharNum > 128) {
     alert("The password is not between the parameteres 8 and 128. Please try again.");
     var passCharNum = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
-
 }
+  
 
+// used to remind the user the parameters of the application
 else if (lowerChoice === false && upperChoice === false && numChoice && specialChoice === false){
   alert("You must select at least 1 option to generate a password.");
   var lowerChoice = confirm("Would you like your password to contain lowercase characters?");
@@ -39,24 +39,27 @@ else if (lowerChoice === false && upperChoice === false && numChoice && specialC
 if (upperChoice){
   blankVariable = blankVariable.concat(upper);
 }
-
 if (lowerChoice){
   blankVariable = blankVariable.concat(lower);
 }
-
 if (numChoice){
-  blankVariable = blankVariable.concat(upper);
+  blankVariable = blankVariable.concat(number);
+}
+if (specialChoice){
+  blankVariable = blankVariable.concat(special);
 }
 
-if (specialChoice){
-  blankVariable = blankVariable.concat(upper);
-}
+//use math and random tools to generate a random password string from the data input
 
 
 // write password to the #password input
-function writePassword() {
-var password = generatePassword() ;
-var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
+  var password = generatePassword() ;
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
 }
+
+writePassword();
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
